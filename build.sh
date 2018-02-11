14c4b1245a86a8d6444afbc4a121a6be4821f08a
pustovitDmytro <pustic@gmail.com>
2018-02-11 14:12:16 +0200

    date log moved

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 git checkout -b travis_tmp
-date '+%A (%e %B)' >> README.md
 sh tmp/git.sh
 
 echo 'done!'
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -e
 git remote rm origin
 git remote add origin https://user-name:$GITHUB-TOKEN@github.com/pustovitDmytro/contributions.git
+date '+%A (%e %B)' >> README.md
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
 git branch -m travis_tmp travis_deploy
 git push -f origin travis_deploy
