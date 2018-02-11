bef614aa3f9f4288003f8caa731e7d144065d076
pustovitDmytro <pustic@gmail.com>
2018-02-11 15:16:12 +0200

    username and token

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 git remote rm origin
-git remote add origin https://user-name:e6b99706bd4bdb7875737d15056b450d68e520d3@github.com/pustovitDmytro/contributions.git
+git remote add origin https://pustovitDmytro:$token@github.com/pustovitDmytro/contributions.git
 date '+%A (%e %B)' >> README.md
 git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
