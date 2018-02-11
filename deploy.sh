75052a00d7188beb06784567c2a3afd71d76b39f
pustovitDmytro <pustic@gmail.com>
2018-02-11 14:35:43 +0200

    token escaped

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 git remote rm origin
-git remote add origin https://user-name:$GITHUB-TOKEN@github.com/pustovitDmytro/contributions.git
+git remote add origin https://user-name:$token@github.com/pustovitDmytro/contributions.git
 date '+%A (%e %B)' >> README.md
 git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
