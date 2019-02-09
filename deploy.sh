5a9fff39fb8237db822bf524a518a17bdd3fa909
Dmytro <dipustovit@gmail.com>
2019-02-09 16:51:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git branch -m travis_tmp travis_deploy
 git push -f origin travis_deploy
 curl -X PATCH \
   https://api.github.com/repos/pustovitDmytro/contributions \
-  -H 'Authorization: token $token' \
+  -H "Authorization: token $token" \
   -H 'Content-Type: application/json' \
   -d '{"default_branch": "travis_deploy"}'
