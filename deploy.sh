2b426c38f7ccf0a8371df3b76e0a30d5e89ad9b3
Dmytro <dipustovit@gmail.com>
2019-02-09 16:46:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,8 @@ git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
 git branch -m travis_tmp travis_deploy
 git push -f origin travis_deploy
+curl -X PATCH \
+  https://api.github.com/repos/pustovitDmytro/contributions \
+  -H 'Authorization: token $token' \
+  -H 'Content-Type: application/json' \
+  -d '{"default_branch": "travis_deploy"}'
