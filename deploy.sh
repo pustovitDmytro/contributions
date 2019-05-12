5c4a5770860d0a8fd77ae60c55e1016e899ddccd
Dmytro <dipustovit@gmail.com>
2019-05-12 13:06:44 +0300

    changing default branch for github preview refreshing

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,14 @@ date '+%A (%e %B)' >> README.md
 git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
 git branch -m travis_tmp travis_deploy
+curl -X PATCH \
+  https://api.github.com/repos/pustovitDmytro/contributions \
+  -H "Authorization: token $token" \
+  -H 'Content-Type: application/json' \
+  -d '{"name":"contributions", "default_branch": "master"}'
 git push -f origin travis_deploy
 curl -X PATCH \
   https://api.github.com/repos/pustovitDmytro/contributions \
   -H "Authorization: token $token" \
   -H 'Content-Type: application/json' \
-  -d '{"default_branch": "travis_deploy"}'
+  -d '{"name":"contributions", "default_branch": "travis_deploy"}'
